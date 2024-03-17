b927411e050c2a082f74c724b2275114a9768a58
vasilemosan <vmctsp@gmail.com>
2024-03-17 10:22:01 +0200

    adding chromesettings

--- src/test/java/ManageChromeSettings.java
+++ src/test/java/ManageChromeSettings.java
@@ -1,12 +1,17 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class ManageChromeSettings {
     public static String baseUrl = "https://www.automationtesting.co.uk/";
@@ -51,7 +56,29 @@ public class ManageChromeSettings {
 
     @Test
     public void testChromeCookies() throws InterruptedException{
-        driver.get("https://expired.badssl.com/");
-        Thread.sleep(5000);
+        driver.get("http://teststore.automationtesting.co.uk/");
+        driver.findElement(By.cssSelector("[title] .hidden-sm-down")).click();
+        Thread.sleep(500);
+        driver.findElement(By.cssSelector("section input[name='email']")).sendKeys("test@test.com");
+        Thread.sleep(500);
+        driver.findElement(By.cssSelector("input[name='password']")).sendKeys("test123");
+        Thread.sleep(500);
+        driver.findElement(By.cssSelector("button#submit-login")).click();
+        Thread.sleep(500);
+        driver.findElement(By.linkText("CLOTHES")).click();
+        Thread.sleep(500);
+        driver.findElement(By.cssSelector("img[alt='Hummingbird printed t-shirt']")).click();
+        Thread.sleep(500);
+        driver.findElement(By.cssSelector(".add-to-cart.btn.btn-primary")).click();
+        //Explicit waits, are set to individual locators, every 500ms element is checked
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(
+                "#blockcart-modal > div > div > div.modal-body > div > div.col-md-7 > div > div > button"))).click();
+        Thread.sleep(1000);
+
+        driver.manage().deleteAllCookies();
+
+        driver.navigate().refresh();
+        Thread.sleep(3000);
     }
 }
