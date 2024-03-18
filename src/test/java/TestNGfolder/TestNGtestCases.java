ba9a6b1d559d2c8cf71553de2225d4984b121a0e
vasile mosan <vasile@vasiles-MacBook-Pro.local>
2024-03-18 18:33:56 +0200

    added TestNG config and test cases

---
+++ src/test/java/TestNGfolder/TestNGtestCases.java
@@ -0,0 +1,10 @@
+package TestNGfolder;
+
+import org.testng.annotations.Test;
+
+public class TestNGtestCases {
+    @Test
+    void test1(){
+        System.out.println("test the TestNG");
+    }
+}
---
+++ src/test/java/TestNGfolder/TestNGxml.java
@@ -0,0 +1,16 @@
+package TestNGfolder;
+
+import org.testng.annotations.Test;
+
+public class TestNGxml {
+
+    @Test
+    public void method1(){
+        System.out.println("Hello from method1");
+    }
+
+    @Test
+    public void method2(){
+        System.out.println("Hello from method2");
+    }
+}
